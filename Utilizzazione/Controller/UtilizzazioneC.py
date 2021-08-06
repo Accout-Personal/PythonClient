abd1ded9219ff90513175964d534613a9e7747ce
Zhang <30994021+Accout-Personal@users.noreply.github.com>
2021-08-06 17:04:07 +0200

    utilizzazione

--- Utilizzazione/Controller/UtilizzazioneC.py
+++ Utilizzazione/Controller/UtilizzazioneC.py
@@ -1,34 +1,48 @@
 import requests
 import env
 
+
 class UtilizzazioneC:
 
     def __init__(self):
-        self.credenziali = {"Authorization": "Bearer "+env.token,"Accept": env.impostazione}
+        self.credenziali = {"Authorization": "Bearer " +
+                            env.token, "Accept": env.impostazione}
         self.url = "gen/pro/resp/utilizzazione/"
         self.url1 = "gen/pro/utilizzazione/"
-    
+
     def GetAll(self):
-        response = requests.get(env.host + env.Url+self.url,headers=self.credenziali)
+        response = requests.get(
+            env.host + env.Url+self.url, headers=self.credenziali)
         if response.ok:
             print("va bene")
             return response.json()
         else:
             return response.json()
-            raise Exception("Errore. ",response.status_code)
-            
-    def GetKey(self,key):
-        response = requests.get(env.host + env.Url+self.url+'key/'+key,headers=self.credenziali)
+            raise Exception("Errore. ", response.status_code)
+
+    def GetKey(self, key):
+        response = requests.get(
+            env.host + env.Url+self.url+'key/'+key, headers=self.credenziali)
         if response.ok:
             print("va bene")
             return response.json()
         else:
-            raise Exception("Errore. ",response.status_code)
-    
+            raise Exception("Errore. ", response.status_code)
+
     def GetSelf(self):
-        response = requests.get(env.host + env.Url+self.url1+'self/',headers=self.credenziali)
+        response = requests.get(
+            env.host + env.Url+self.url1+'self/', headers=self.credenziali)
         if response.ok:
             print("va bene")
             return response.json()
         else:
-            raise Exception("Errore. ",response.status_code)
+            raise Exception("Errore. ", response.status_code)
+
+    def Insert(self, body):
+        response = requests.post(
+            env.host + env.Url+self.url1+'insert', data=body, headers=self.credenziali)
+        if response.ok:
+            print("va bene")
+            return response.json()
+        else:
+            raise Exception("Errore. ", response.status_code)
