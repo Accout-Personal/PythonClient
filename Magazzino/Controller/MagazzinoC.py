1a8d5f482edd85756dbdfcfc141129bd3627dca2
simone <s1088431@studenti.univpm.it>
2021-08-06 16:59:21 +0200

    ok

--- Magazzino/Controller/MagazzinoC.py
+++ Magazzino/Controller/MagazzinoC.py
@@ -2,13 +2,20 @@ import requests
 import env
 
 class MagazzinoC:
-    def __init__(self,token):
-        self.token =  token
-        self.credenziali = {"Authorization": "Bearer "+self.token,"Accept": env.impostazione}
+    def __init__(self):
+        self.credenziali = {"Authorization": "Bearer "+env.token,"Accept": env.impostazione}
         self.url = "magazzino/"
     def GetAll(self):
         response = requests.get(env.host+env.Url+self.url,headers=self.credenziali)
         if response.ok:
             print(response.json())
+        else:
+            raise Exception("Errore. ",response.status_code)
+
+    def Insert(self,request):
+        response = requests.post(env.host + env.Url+"magazzino/insert",headers=self.credenziali,data=request)
+        if response.ok:
+            print("va bene")
+            return response.json()
         else:
             raise Exception("Errore. ",response.status_code)
