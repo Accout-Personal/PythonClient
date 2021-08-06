13c2c992d57338abc2caa4e391e42c86778b3e05
Zhang <30994021+Accout-Personal@users.noreply.github.com>
2021-08-06 16:43:42 +0200

    entrata insert

--- Entrata/Controller/EntrataC.py
+++ Entrata/Controller/EntrataC.py
@@ -23,9 +23,10 @@ class EntrataC:
             raise Exception("Errore. ",response.status_code)
 
     def Insert(self,body):
-        response = requests.post(env.host + env.Url+self.url1+'insert',data=body,headers=self.credenziali)
+        response = requests.post(env.host + env.Url+self.url+'insert',data=body,headers=self.credenziali)
         if response.ok:
             print("va bene")
             return response.json()
         else:
+            return response.json()
             raise Exception("Errore. ",response.status_code)
