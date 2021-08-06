6e85f36e3cd26ac138915ca38dfc0878e18a0deb
Zhang <30994021+Accout-Personal@users.noreply.github.com>
2021-08-06 17:03:57 +0200

    non contabile

--- NonContabile/Controller/NonContabileC.py
+++ NonContabile/Controller/NonContabileC.py
@@ -21,11 +21,12 @@ class NonContabileC:
             return response.json()
         else:
             raise Exception("Errore. ",response.status_code)
-            
+
     def Insert(self,body):
-        response = requests.post(env.host + env.Url+self.url1+'insert',data=body,headers=self.credenziali)
+        response = requests.post(env.host + env.Url+self.url+'insert',data=body,headers=self.credenziali)
         if response.ok:
             print("va bene")
             return response.json()
         else:
+            return response.json()
             raise Exception("Errore. ",response.status_code)
