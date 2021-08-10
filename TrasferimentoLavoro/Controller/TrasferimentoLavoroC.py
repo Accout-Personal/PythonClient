4c834685a71d2e2beac1addafafd3a05acb58ad4
simone <s1088431@studenti.univpm.it>
2021-08-10 21:15:55 +0200

    ok

--- TrasferimentoLavoro/Controller/TrasferimentoLavoroC.py
+++ TrasferimentoLavoro/Controller/TrasferimentoLavoroC.py
@@ -40,4 +40,19 @@ class TrasferimentoLavoroC:
         else:
             return response.json()
 
+    def Delete(self,body):
+        response = requests.post(env.host + env.Url+self.url+'delete',data=body,headers=self.credenziali)
+        if response.ok:
+            print("va bene")
+            return response.json()
+        else:
+            return response.json()
     
+    def Update(self,body,body2):
+        response = requests.post(env.host + env.Url+self.url+'update',data=body,headers=self.credenziali)
+        if response.ok:
+            print("va bene")
+            response2 = requests.post(env.host + env.Url+self.url+'update',data=body2,headers=self.credenziali)
+            return response.json()
+        else:
+            return response.json()
