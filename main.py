e826d2cd087abf094bbd76d1713b8c831775166a
simone <s1088431@studenti.univpm.it>
2021-08-10 16:48:23 +0200

    ook

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ListinoPrezzi.Controller.ListinoPrezziC import ListinoPrezziC
 from Commessa.Controller.CommessaC import CommessaC
 from Ddt.Controller.DdtC import DdtC
 from TrasferimentoLavoro.Controller.TrasferimentoLavoroC import TrasferimentoLavoroC
@@ -9,6 +10,6 @@ from Utilizzazione.Controller.UtilizzazioneC import UtilizzazioneC
 from Aggiornamento.Controller.AggiornamentoC import AggiornamentoC
 
 
-prova = CommessaC()
-dizionario = {'tessuto': 'SIMO33','numero_commessa': '27','Qta': '500','listino_prezzi_modello': '11200','nddt_cliente': '555','data': '2021/08/06','cliente': '10203044557'}
-print(prova.GetAll())
+prova = ListinoPrezziC()
+dizionario = {'modello': '456pippo','valore_cliente': '11','valore_dipendente': '7'}
+print(prova.Insert(dizionario))
