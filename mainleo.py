a932ec84adc84c2e9fb023311f8846d4a531a601
Zhang <30994021+Accout-Personal@users.noreply.github.com>
2021-08-10 16:58:46 +0200

    stipendioC

--- mainleo.py
+++ mainleo.py
@@ -119,4 +119,15 @@ retribuzione_insert ={
     'dipendente_retr':'PPRRCN88C02H501O'
 }
 
-print(RetribuzioneVariabileC().Insert(retribuzione_insert))
+#print(RetribuzioneVariabileC().Insert(retribuzione_insert))
+
+
+stipendio_insert ={
+    'data':'2021-08-10',
+    'dipendente_stip':'PPRRCN88C02H501O',
+    'giorni_ferie':1,
+    'ore_straordinario':20,
+    'ore_lavoro':50
+}
+
+#print(StipendioC().Insert(stipendio_insert))
