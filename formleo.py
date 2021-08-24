6a79168ccef583adfe074c4068c599882296e63f
Leo <494671601a@gmail.com>
2021-08-24 19:27:59 +0200

    form leo python variable button

--- formleo.py
+++ formleo.py
@@ -8,46 +8,49 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QWidget
+import copy
 
 class Ui_UIwindow(QWidget):
     def __init__(self, parent=None):
         super(Ui_UIwindow, self).__init__(parent)
+        self.pushButton = []
         UIwindow = self
         UIwindow.setObjectName("UIwindow")
-        UIwindow.resize(222, 209)
+        UIwindow.resize(312, 362)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(UIwindow.sizePolicy().hasHeightForWidth())
+        UIwindow.setSizePolicy(sizePolicy)
+        self.formLayout = QtWidgets.QFormLayout(UIwindow)
+        self.formLayout.setObjectName("formLayout")
         self.scrollArea = QtWidgets.QScrollArea(UIwindow)
-        self.scrollArea.setGeometry(QtCore.QRect(30, 30, 141, 141))
+        self.scrollArea.setAcceptDrops(False)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 139, 139))
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 102, 303))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.scrollAreaWidgetContents.sizePolicy().hasHeightForWidth())
+        self.scrollAreaWidgetContents.setSizePolicy(sizePolicy)
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.pushButton = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton.setGeometry(QtCore.QRect(20, 10, 80, 25))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_2.setGeometry(QtCore.QRect(20, 100, 80, 25))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_3.setGeometry(QtCore.QRect(20, 60, 80, 25))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_4.setGeometry(QtCore.QRect(20, 140, 80, 25))
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_5 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_5.setGeometry(QtCore.QRect(20, 190, 80, 25))
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        _translate = QtCore.QCoreApplication.translate
 
-        self.retranslateUi(UIwindow)
+        #UIwindow
+        for i in range(20):
+            #print(i)
+            self.pushButton.append(QtWidgets.QPushButton(self.scrollAreaWidgetContents))
+            self.pushButton[i].setObjectName("pushButton"+str(i))
+            self.gridLayout_2.addWidget(self.pushButton[i], i, 0, 1, 1)
+            self.pushButton[i].setText(_translate("UIwindow", "PushButton"+str(i)))
+            self.pushButton[i].clicked.connect(lambda state,a=i: self.printbutton(a))
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.scrollArea)
         QtCore.QMetaObject.connectSlotsByName(UIwindow)
 
-    def retranslateUi(self, UIwindow):
-        _translate = QtCore.QCoreApplication.translate
-        UIwindow.setWindowTitle(_translate("UIwindow", "UIwindow"))
-        self.pushButton.setText(_translate("UIwindow", "PushButton"))
-        self.pushButton_2.setText(_translate("UIwindow", "PushButton"))
-        self.pushButton_3.setText(_translate("UIwindow", "PushButton"))
-        self.pushButton_4.setText(_translate("UIwindow", "PushButton"))
-        self.pushButton_5.setText(_translate("UIwindow", "PushButton"))
-
+    def printbutton(self,i):
+        print(i)
--- mainQTLeo.py
+++ mainQTLeo.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 #from formleo import Ui_UIwindow
-from formpython import Ui_UIwindow
+from formleo import Ui_UIwindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
