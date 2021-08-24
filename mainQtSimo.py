6f5af1dff91c05b5583135307640246670665d1d
simone <s1088431@studenti.univpm.it>
2021-08-24 16:43:11 +0200

    ok

--- mainQtSimo.py
+++ mainQtSimo.py
@@ -1,9 +1,10 @@
+from Login.VistaLogin import Ui_VistaLog
 from formpythonsimo3 import Ui_UIwindow
 import sys
 from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    vista_home = Ui_UIwindow()
+    vista_home = Ui_VistaLog()
     vista_home.show()
     sys.exit(app.exec())
