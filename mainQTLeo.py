fdd25306a63cfb41c2ceea3b6f173f7ff500604a
Leo <494671601a@gmail.com>
2021-08-27 18:17:27 +0200

    vista anagrafica

--- mainQTLeo.py
+++ mainQTLeo.py
@@ -3,10 +3,11 @@ from PyQt5.QtWidgets import QApplication
 
 #from formleo import Ui_UIwindow
 from formleo import Ui_UIwindow
+from Login.VistaLogin.formLogPython import Ui_VistaLog
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    vista_home = Ui_UIwindow()
+    vista_home = Ui_VistaLog()
     vista_home.show()
     sys.exit(app.exec())
 
