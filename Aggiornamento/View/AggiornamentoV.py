3e0a02b717f7194e9b3a91beaed11a046a444f99
simone <s1088431@studenti.univpm.it>
2021-09-06 00:34:13 +0200

    ok

--- Aggiornamento/View/AggiornamentoV.py
+++ Aggiornamento/View/AggiornamentoV.py
@@ -34,7 +34,7 @@ class Ui_ListaAggiornamenti(QWidget):
 
         self.traduzione = {
         'CF':'Codice fiscale',
-        'nome_cognome':'nome e cognome',
+        'nome_cognome':'Nome e cognome',
         'data':'Data',
         'quantita':'Quantità',
         'magazzino':'Articolo',
@@ -42,7 +42,7 @@ class Ui_ListaAggiornamenti(QWidget):
 
         self.listAttr = ['CF','nome_cognome','data','quantita','magazzino']
         #le dimensioni delle colonne
-        sizes = [100,100,85,50,100]
+        sizes = [120,110,100,35,110]
         for attr in range(len(self.listAttr)):
             
             #Aggiunge l'intestazione della tabella
@@ -78,11 +78,11 @@ class Ui_ListaAggiornamenti(QWidget):
         QtCore.QMetaObject.connectSlotsByName(ListaAggiornamenti)
 
     #chiamata alla funzione per la cancellazione di un elemento
-    def deleteConfirm(self,cliente):
+    def deleteConfirm(self,aggiornamento):
 
         msg = QMessageBox()
         msg.setWindowTitle('Conferma')
-        msg.setText('sei sicuro di voler cancellare il Cliente '+cliente['nome_azienda'] + '?')
+        msg.setText("sei sicuro di voler cancellare l' Aggiornamento "+str(aggiornamento['id']) + ' ?')
         msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
         okButton = msg.button(QMessageBox.Yes)
         noButton = msg.button(QMessageBox.No)
@@ -90,7 +90,7 @@ class Ui_ListaAggiornamenti(QWidget):
         retval = msg.exec_()
         if(msg.clickedButton() == okButton):
             print('cancellazione confermata')
-            postbody = {'PIVA':cliente['PIVA']}
+            postbody = {'id':aggiornamento['id']}
             res = self.controller.Delete(postbody)
             print(res)
             self.RefreshLista = Ui_ListaAggiornamenti()
